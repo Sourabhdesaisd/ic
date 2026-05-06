465fd5c56f3f3ed878980eef836c018fb7d50b3b
sourabhkyros <sourabh.desai@kyros-semi.com>
2026-05-06 17:41:03 +0530

    Initial commit1

--- verification/int_scoreboard.sv
+++
@@ -1,9 +0,0 @@
-class int_scoreboard extends uvm_scoreboard;
-
-  `uvm_component_utils(int_scoreboard)
-
-  function new(string name = "int_scoreboard", uvm_component parent);
-    super.new(name, parent);
-  endfunction
-
-endclass
--- verification/int_sequencer.sv
+++
@@ -1,9 +0,0 @@
-class int_sequencer extends uvm_sequencer #(int_seq_item);
-
-  `uvm_component_utils(int_sequencer)
-
-  function new(string name = "int_sequencer", uvm_component parent);
-    super.new(name, parent);
-  endfunction
-
-endclass
