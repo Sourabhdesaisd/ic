386fffbd4c63e04e5c21a79bf30d06d8537cf0f2
sourabhkyros <sourabh.desai@kyros-semi.com>
2026-06-04 16:37:09 +0530

    Initial commit

--- uvm/sequencer.sv
+++
@@ -1,8 +0,0 @@
-class ic_sequencer extends uvm_sequencer #(ic_txn);
-  `uvm_component_utils(ic_sequencer)
-
-  function new(string name, uvm_component parent);
-    super.new(name, parent);
-  endfunction
-
-endclass
--- uvm/virtual_sequencer.sv
+++
@@ -1,8 +0,0 @@
-class ic_virtual_sequencer extends uvm_sequencer #(uvm_sequence_item);
-  `uvm_component_utils(ic_virtual_sequencer)
-
-  function new(string name, uvm_component parent);
-    super.new(name, parent);
-  endfunction
-
-endclass
